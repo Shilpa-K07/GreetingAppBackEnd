e7a6674a9331a8df49b1d08bb2f090ea0990e9c1
Shilpa-K07 <shilpa07udupi@gmail.com>
2020-11-29 20:58:27 +0530

    refactor: merge with development

--- Greeting-Messaging-App/logger.js
+++ Greeting-Messaging-App/logger.js
@@ -2,8 +2,8 @@ const winston = require('winston')
 const logger = winston.createLogger({
     format : winston.format.json(),
     transports : [
-        new winston.transports.File({level : 'error',filename : 'error.log'}),
-        new winston.transports.File({filename : 'generalLogs.log'}),
+        new winston.transports.File({level : 'error',filename : './log/error.log'}),
+        new winston.transports.File({filename : './log/generalLogs.log'}),
     ]
 });
 
